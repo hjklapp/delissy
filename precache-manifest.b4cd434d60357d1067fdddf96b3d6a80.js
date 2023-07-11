54d757bee83d7e91a2fa2b289174ec1d87d65e7f
hjklapp <34714378+hjklapp@users.noreply.github.com>
2023-07-11 15:27:25 +0200

    Updates

---
+++ precache-manifest.b4cd434d60357d1067fdddf96b3d6a80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbb9b76c464d1cff0805",
+    "url": "/delissy/static/css/main.98fdcba8.chunk.css"
+  },
+  {
+    "revision": "dbb9b76c464d1cff0805",
+    "url": "/delissy/static/js/main.dbb9b76c.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/delissy/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/delissy/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "a688c03c046950c31871",
+    "url": "/delissy/static/js/runtime~main.a688c03c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/delissy/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "77b5fd0135985dd98ac14e2a53a46d58",
+    "url": "/delissy/index.html"
+  }
+];
